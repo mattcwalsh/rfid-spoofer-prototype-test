fbf6f460a63ad76ffbbb9fdb8a4d7383569a0602
mattcwalsh <rothcolt.d03@gmail.com>
2019-11-27 21:43:34 -0800

    Restructure for AtmelStudio

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * rfid-spoofer-prototype.cpp
+ *
+ * Created: 11/27/2019 9:34:01 PM
+ * Author : mwrot
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
